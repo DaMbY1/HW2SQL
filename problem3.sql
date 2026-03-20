f0447098b3d24c4e35b0adbf62324c7ec3b47da4
DaMbY1 <dmitrybab80@mail.ru>
2026-03-20 15:34:56 +0300

    Update problem3.sql

--- problem3.sql
+++ problem3.sql
@@ -1,5 +1,4 @@
 WITH monthly_sales AS (
-    -- Группируем по товару и месяцу. Решение работает в рамках предположения о том, что все происходит в пределах одного года
     SELECT 
         product_id,
         DATE_TRUNC('month', sale_date) AS month,
@@ -11,15 +10,13 @@ SELECT
     product_id,
     month,
     monthly_amount,
-    -- Процент изменения относительно предыдущего месяца
-    CASE 
-        WHEN LAG(monthly_amount) OVER (PARTITION BY product_id ORDER BY month) IS NOT NULL 
-        THEN ROUND(
+    COALESCE(
+        ROUND(
             (monthly_amount - LAG(monthly_amount) OVER (PARTITION BY product_id ORDER BY month)) * 100.0 
             / LAG(monthly_amount) OVER (PARTITION BY product_id ORDER BY month), 
             2
-        )
-        ELSE NULL  -- для первого месяца нет предыдущего
-    END AS percent_change
+        ),
+        0
+    ) AS percent_change
 FROM monthly_sales
 ORDER BY product_id, month;
